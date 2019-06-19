2dc83bf74b70c3a10096528de35a15b8f31e817f
BugTan <bugtan@vip.qq.com>
2019-06-19 16:05:12 +0800

    first commit

---
+++ genDocx.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2019年6月12日
+
+@author: BugTan
+'''
+
+
+from docx import Document
+
+document = Document()
+document.add_heading('T彭婷是个大傻子ing of the universe')
+document.add_heading('The role of dolphins', level=2)
+paragraph = document.add_paragraph('Lorem ipsum dolor sit amet.')
+
+paragraph = document.add_paragraph('dadasdasd')
+
+document.save('test.docx')
